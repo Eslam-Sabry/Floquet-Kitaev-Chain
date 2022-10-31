0c1dd38a885b90ce8b2ba7dbee5a3cb38632cf47
mtsoltan <mahmoud.t.soltan@gmail.com>
2022-10-31 20:52:11 +0200

    format code, add temporary todos, minor fixes

--- floquet.py
+++ floquet.py
@@ -1,47 +1,46 @@
 from functools import reduce
-import imp
 import numpy as np
 from constants import *
 from green_function import *
-#import holoviews
+# import holoviews
 from scipy import linalg as sla
 import matplotlib as mp
-#plt.style.use('seaborn')		# Setting the plotting style
-mp.rcParams['figure.figsize'] = (20, 10)  # Setting the size of the plots
-import numpy.linalg as la 
+import numpy.linalg as la
 from scipy.integrate import quad
+
+# plt.style.use('seaborn')		# Setting the plotting style
+mp.rcParams['figure.figsize'] = (20, 10)  # Setting the size of the plots
 
 
 def one_period_propagator(hamiltonians, T, n=None):
     if isinstance(hamiltonians, list):
-        num = len(hamiltonians)
-        exps = [sla.expm(-1j * h * T / num) for h in hamiltonians]
-        return reduce(np.matmul, exps)
-
+        n = len(hamiltonians)
     elif callable(hamiltonians):
-        hamiltonian = [hamiltonians(i*(T/n)) for i in range(n)]
-        exps = [sla.expm(-1j * h * T / n) for h in hamiltonian]
-        return reduce(np.matmul, exps)
+        hamiltonians = [hamiltonians(i * (T / n)) for i in range(n)]
 
-def floquet_hamiltonian(hamiltonians,T,n=None):
-    u_T = one_period_propagator(hamiltonians,T,n)
-    eval, evec= la.eig(u_T)
-    h_f1 =evec @ np.diag(np.log(eval)*1j/np.pi) @ la.inv(evec)
+    exps = [sla.expm(-1j * h * T / n) for h in hamiltonians]
+    return reduce(np.matmul, exps)
+
+
+def floquet_hamiltonian(hamiltonians, T, n=None):
+    u_T = one_period_propagator(hamiltonians, T, n)
+    eigenval, eigenvec = la.eig(u_T)
+    h_f1 = eigenvec @ np.diag(np.log(eigenval) * 1j / np.pi) @ la.inv(eigenvec)
     return h_f1
-    #return 1j * sla.logm(u_T)/np.pi    #this is slow. better diagonalize then take the logarithm 
+    # return 1j * sla.logm(u_T)/np.pi    #this is slow. better diagonalize then take the logarithm
 
 
-#فكرة فاشلة
-#def propagator(hamiltonian,time,N,d,order=10,**kwargs):
+# فكرة فاشلة
+# def propagator(hamiltonian,time,N,d,order=10,**kwargs):
 #    u = lambda x: np.eye(d*N,dtype=complex)
 #    for i in range(order):
 #        u = lambda x: u(x) - i * np.vectorize(quad)(lambda t: hamiltonian(t,kwargs)@u(t), 0, x)
 #    return u(time)
 
 
-
 def calculate_finite_spectrum(periods, hamiltonians):
     energies = []
+    # TODO: I need to understand what is going on here. This looks very strange.
     for T in periods:
         U = one_period_propagator(hamiltonians, T)
         phases = np.angle(la.eigvals(U))
@@ -53,6 +52,7 @@ def calculate_finite_spectrum(periods, hamiltonians):
 
 def calculate_bands(momenta, hamiltonians_k, T):
     energies = []
+    # TODO: I need to understand what is going on here. This looks very strange.
     for k in momenta:
         hamiltonians = [h_k(k) for h_k in hamiltonians_k]
         U = one_period_propagator(hamiltonians, T)
@@ -62,42 +62,47 @@ def calculate_bands(momenta, hamiltonians_k, T):
         energies.append(ev)
     return np.array(energies).real
 
-def plot_spectrum(Hmat,N=N):
-    evals,evecs = la.eigh(Hmat)
+
+# TODO: Name shadowing may cause issues.
+def plot_spectrum(Hmat, N=N):
+    evals, evecs = la.eigh(Hmat)
     evals = evals.real
-    plt.scatter(np.arange(len(evals)),evals)
+    plt.scatter(np.arange(len(evals)), evals)
     plt.title('Energy Spectrum of Chain with {} Sites'.format(N))
     plt.show()
-    #plt.savefig("spectrum")
+    # plt.savefig("spectrum")
 
 
+# TODO: Not used!
 e_threshold = 1E-6
 
 
-def check_modes(evals, mode_energy,e_threshold=None):
+# TODO: Name shadowing may cause issues. Does not default to outer e_threshold.
+def check_modes(evals, mode_energy, e_threshold=None):
+    # TODO: Unused variable
     nzmodes = 0
-    if e_threshold == None:
+    if e_threshold is None:
         zmodes_ind = np.where(np.isclose(abs(evals), mode_energy * np.ones(len(evals))))[0]
     else:
-        zmodes_ind = np.where(np.isclose(abs(evals), mode_energy * np.ones(len(evals)),atol=e_threshold))[0]
-    return zmodes_ind,len(zmodes_ind)
-    
+        zmodes_ind = np.where(np.isclose(abs(evals), mode_energy * np.ones(len(evals)), atol=e_threshold))[0]
+    return zmodes_ind, len(zmodes_ind)
 
-def plot_modes(evals,evecs, mode_energy,e_threshold=None):
-    modes_ind,cnt_modes = check_modes(evals, mode_energy,e_threshold=e_threshold)
+
+# TODO: Name shadowing may cause issues. Does not default to outer e_threshold.
+def plot_modes(evals, evecs, mode_energy, e_threshold=None):
+    modes_ind, cnt_modes = check_modes(evals, mode_energy, e_threshold=e_threshold)
     if cnt_modes > 0:
-        fig,ax = plt.subplots(1,cnt_modes,figsize=(20, 10))
-        fig.suptitle('Probability distribution of $E = \pm {}$ modes'.format(mode_energy),fontsize=20, fontweight='bold')
+        fig, ax = plt.subplots(1, cnt_modes, figsize=(20, 10))
+        fig.suptitle('Probability distribution of $E = \\pm {}$ modes'.format(mode_energy), fontsize=20,
+                     fontweight='bold')
         for cnt in range(cnt_modes):
             ax1 = ax[cnt]
-            ax1.plot(np.abs(evecs[:,modes_ind[cnt]])**2)
-            ax1.set_title('Edge mode {}'.format(cnt+1),fontsize=20)
-            ax1.set_xlabel('Site Number',fontsize=20)
-            ax1.set_ylabel('$|\psi|^2$',fontsize=20)
-            #ax1.text(0.43, 0.95, param_info, transform=ax1.transAxes, fontsize=16,
-        #verticalalignment=('top', bbox=dict(boxstyle="square",facecolor="white"))
+            ax1.plot(np.abs(evecs[:, modes_ind[cnt]]) ** 2)
+            ax1.set_title('Edge mode {}'.format(cnt + 1), fontsize=20)
+            ax1.set_xlabel('Site Number', fontsize=20)
+            ax1.set_ylabel('$|\\psi|^2$', fontsize=20)
+            # ax1.text(0.43, 0.95, param_info, transform=ax1.transAxes, fontsize=16,
+            # verticalalignment=('top', bbox=dict(boxstyle="square",facecolor="white"))
             ax1.tick_params(axis='both', which='major', labelsize=16)
-        #plt.savefig('Edge_modes_Kitaev.pdf')
+        # plt.savefig('Edge_modes_Kitaev.pdf')
         plt.show()
-
-
--- kitaev.py
+++ kitaev.py
@@ -5,61 +5,77 @@ from green_function import *
 from tqdm import tqdm
 from scipy import linalg as sla
 import matplotlib as mp
-plt.style.use('seaborn')		# Setting the plotting style
-mp.rcParams['figure.figsize'] = (15, 10)  # Setting the size of the plots
-import numpy.linalg as la 
+
+import numpy.linalg as la
 from floquet import *
 from scipy.integrate import quad
 from scipy import fft
 
+plt.style.use('seaborn')  # Setting the plotting style
+mp.rcParams['figure.figsize'] = (15, 10)  # Setting the size of the plots
 
-def onsite(mu=mu, **kwargs):
+
+# TODO: Name shadowing may cause issues.
+def onsite(mu=mu):
     return -mu * s_z
 
 
-def offsite(t=t, delta=delta,**kwargs):
-    return -t * s_z +  1j * delta * s_y
+# TODO: Name shadowing may cause issues.
+def offsite(t=t, delta=delta):
+    return -t * s_z + 1j * delta * s_y
 
 
 N = 2
 
 periods = np.linspace(0.2 / t, 5 / t, 100)
-momenta = np.linspace(-2*np.pi, 2*np.pi,100)
+momenta = np.linspace(-2 * np.pi, 2 * np.pi, 100)
 
-Delta = 1*t
+Delta = 1 * t
 
-h_1 = Hamiltonian(d,N,onsite(mu=0*t),offsite(delta=Delta))
-h_2 = Hamiltonian(d,N,onsite(mu=1*t),offsite(delta=Delta))
+h_1 = Hamiltonian(d, N, onsite(mu=0 * t), offsite(delta=Delta))
+h_2 = Hamiltonian(d, N, onsite(mu=1 * t), offsite(delta=Delta))
 
-h = Hamiltonian(d,N,onsite,offsite)
+h = Hamiltonian(d, N, onsite, offsite)
 
-
-
-#a = np.array([[np.sin,np.abs],[np.cos,lambda x: 1]])
-#ans = np.vectorize(quad)(a,0,np.pi)
-#print(ans[0])
+# TODO: Commented
+# a = np.array([[np.sin,np.abs],[np.cos,lambda x: 1]])
+# ans = np.vectorize(quad)(a,0,np.pi)
+# print(ans[0])
 T = 0.5
-def kitaev_hamiltonian(time,period = T,h_1=h_1,h_2=h_2,**kwargs):
-    if time < period/2:
+
+
+# TODO: Name shadowing may cause issues.
+# TODO: This function is not used
+def kitaev_hamiltonian(time, period=T, h_1=h_1, h_2=h_2):
+    if time < period / 2:
         return h_1.lattice_hamiltonian()
     else:
         return h_2.lattice_hamiltonian()
 
 
-#u = propagator(hamiltonian, T,N,d)
+# TODO: Commented
+# u = propagator(hamiltonian, T,N,d)
 
-def kitaev_propagator(time,period = T, h_1=h_1,h_2=h_2):
-    if time%period <= period/2:
-        return sla.expm(-1j*h_1.lattice_hamiltonian()*time)
+# TODO: Name shadowing may cause issues.
+def kitaev_propagator(time, period=T, h_1=h_1, h_2=h_2):
+    if time % period <= period / 2:
+        return sla.expm(-1j * h_1.lattice_hamiltonian() * time)
     else:
-        exps = [sla.expm(-1j * h_1.lattice_hamiltonian() * (time - period / 2)) ,sla.expm(-1j * h_2.lattice_hamiltonian() * period / 2)]
-        #return reduce(np.matmul, exps)
-        return np.matmul(sla.expm(-1j * h_1.lattice_hamiltonian() * (time - period / 2)) ,sla.expm(-1j * h_2.lattice_hamiltonian() * period / 2))
+        # TODO: Unused variable
+        exps = [sla.expm(-1j * h_1.lattice_hamiltonian() * (time - period / 2)),
+                sla.expm(-1j * h_2.lattice_hamiltonian() * period / 2)]
+        # TODO: Commented
+        # return reduce(np.matmul, exps)
+        return np.matmul(sla.expm(-1j * h_1.lattice_hamiltonian() * (time - period / 2)),
+                         sla.expm(-1j * h_2.lattice_hamiltonian() * period / 2))
 
-def kitaev_kick_operator(time,period = T, h_1 = h_1, h_2 = h_2):
-    U = kitaev_propagator(time,period=period,h_1=h_1,h_2=h_2)
-    h_f = floquet_hamiltonian([h_1.lattice_hamiltonian(),h_2.lattice_hamiltonian()],T=period)
-    p = np.matmul(U,sla.expm(1j*np.pi*h_f*(time/period)))
+
+# TODO: Name shadowing may cause issues.
+def kitaev_kick_operator(time, period=T, h_1=h_1, h_2=h_2):
+    U = kitaev_propagator(time, period=period, h_1=h_1, h_2=h_2)
+    h_f = floquet_hamiltonian([h_1.lattice_hamiltonian(), h_2.lattice_hamiltonian()], T=period)
+    p = np.matmul(U, sla.expm(1j * np.pi * h_f * (time / period)))
     return p
-    
+
+
 print("hi")
